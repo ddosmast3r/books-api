9e0a78d8236ef24457a9d83ee76503b63bb5f6bc
OdisseyP <pogosov.fortech@gmail.com>
2025-06-16 17:36:06 +0300

    add auth and user modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/user.module';
+import { User } from './users/user.entity';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [User],
+    synchronize: process.env.DB_SYNC === 'true',
+  }), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,28 @@
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+
+export class RegisterDto {
+    @IsEmail()
+    @IsString()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
+    password: string;
+
+    @IsString()
+    firstName: string;
+
+    @IsString()
+    lastName: string;
+}
+
+export class LoginDto {
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
+    password: string;
+}
