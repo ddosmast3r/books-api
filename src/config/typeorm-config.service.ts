d89586459c6438aebec224d4a930466151855ace
Odissey Pogosov <pogosov.fortech@gmail.com>
2025-07-24 17:07:11 +0300

    fix: fix migration comments

--- src/config/typeorm-config.service.ts
+++ src/config/typeorm-config.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import { config } from 'dotenv';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
@@ -17,23 +15,9 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       password: this.configService.get('POSTGRES_PASSWORD'),
       database: this.configService.get('POSTGRES_DB'),
       autoLoadEntities: true,
-      migrations: [__dirname + '/../migrations/*.ts'],
+      migrations: ['src/migrations/*.ts'],
       migrationsRun: true,
       synchronize: this.configService.get('POSTGRES_SYNC'),
     };
   }
 }
-
-config();
-
-export const dataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT!) as number,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
-  synchronize: false,
-});
